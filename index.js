4bd4e06d2e6273c529d86b43625f868ecadbc548
manuelsacaeu123 <93751940+manuelsacaeu123@users.noreply.github.com>
2024-05-04 19:44:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3000; //Como se indico en clase es el puerto donde se puede acceder a la aplicacion y puede ser variante
 
+//array
 let librosBiblicos = [
     { id: 1, nombre: 'Genesis', autor: 'Moises' },
     { id: 2, nombre: 'Exodo', autor: 'Moises' },
     { id: 3, nombre: 'Levitico', autor: 'Moises' },
-    { id: 4, nombre: 'Juan', autor: 'Juan' },
-    { id: 5, nombre: 'Juan 2', autor: 'Juan' },
+    { id: 4, nombre: 'Deuteronomio', autor: 'Moises' },
+    { id: 5, nombre: 'Numeros', autor: 'Moises' },
+    { id: 6, nombre: 'Juan', autor: 'Juan' },
+    { id: 7, nombre: 'Juan 2', autor: 'Juan' },
+    { id: 8, nombre: 'Juan 3', autor: 'Juan'},
+    { id: 9, nombre: 'Judas', autor: 'Judas'},
+    { id: 10, nombre: 'Juan', autor: 'Juan'}
 ];
-
+//manejo de json
 app.use(express.json());
 
 // Endpoint de bienvenida
 app.get('/bienvenida', (req, res) => {
-    res.send('Hola Soy Manuel Saca Eulate y actualmente soy Técnico en Telecomunicaciones \n Hola Soy Marco Antonio Villca y actualmente soy Técnico en Informática');
+    res.send('Hola Soy Jose Manuel Saca Eulate y actualmente soy Técnico en Telecomunicaciones \n Hola Soy Marco Antonio Villca Quispe y actualmente soy Técnico en Informática y computacion');
+});
+//endpoint para obtener todos los libros
+app.get('/libros', (req, res) =>{
+    res.json(librosBiblicos);
 });
 
 // Obtener libros por autor
 app.get('/libros/autor/:autor', (req, res) => {
-    const autor = req.params.autor;
-    const librosPorAutor = librosBiblicos.filter(libro => libro.autor === autor);
-    res.json(librosPorAutor);
+    const autorCapturado = req.params.autor;
+    console.log(autorCapturado);
+    const librosPorAutor = librosBiblicos.filter(libro => libro.autor === autorCapturado);
+    if (librosPorAutor.length > 0) {
+        res.json(librosPorAutor);
+    } else {
+        res.status(404).json ({mensaje : 'El Autor no se encuentra'});
+    }
 });
 
 // Obtener la cantidad total de libros
-app.get('/libros/cantidad-total', (req, res) => {
+app.get('/libros/cantidad', (req, res) => {
     const cantidadTotal = librosBiblicos.length;
-    res.json({ cantidadTotal: cantidadTotal });
+    res.json({ cantidadTotal:   cantidadTotal});
 });
 
 // Obtener libros por nombre que contenga el texto "Juan"
 app.get('/libros/nombre/:nombre', (req, res) => {
     const nombre = req.params.nombre;
-    const librosConNombreJuan = librosBiblicos.filter(libro => libro.nombre.includes(nombre));
+    const librosConNombreJuan = librosBiblicos.filter(libro => libro.nombre.includes(nombre) || libro.autor.includes(nombre));
     res.json(librosConNombreJuan);
 });
 
